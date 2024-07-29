796e6620f949a1be32a2dd94783f3ae33289567b
YashSahaa <136882182+YashSahaa@users.noreply.github.com>
2024-07-29 21:57:43 +0530

    first commit

---
+++ employee-system-api/src/main/java/com/practice/employee_system_api/services/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.practice.employee_system_api.services;
+
+import com.practice.employee_system_api.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+    List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);
+
+}
